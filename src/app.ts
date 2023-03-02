0de191128f7a6e78ca0f53c7c74762ef6db5d6ac
Jenwit Suwech <jenwit.suw@themall.co.th>
2023-03-02 10:51:26 +0700

    Remove unnessesary file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import apm from "./lib/elastic";
 
 async function startServer() {
   const app = express();
-  require("./loader").default({ app });
   app.get("/", (req, res) => {
     res.send("Hello World!");
     res.end();
--- src/loader/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Application as ExpressApplication } from "express";
-import { default as ExpressLoader } from "./express";
-import "./jt400";
-
-export default async ({ expressApp }: { expressApp: ExpressApplication }) => {
-  ExpressLoader({ app: expressApp });
-  console.log("✌️ Express loaded");
-};
